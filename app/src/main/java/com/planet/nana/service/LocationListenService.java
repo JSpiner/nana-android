737cf77be4ffe4d0c6f611ebadb01ff5e6066b4e
jspiner <jspiner@naver.com>
2018-09-01 20:31:36 +0900

    위치정보를 받는 Locationservice 추가

---
+++ app/src/main/java/com/planet/nana/service/LocationListenService.java
@@ -0,0 +1,115 @@
+package com.planet.nana.service;
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.planet.nana.BaseApplication;
+
+public class LocationListenService extends Service {
+
+    private static final String TAG = LocationListenService.class.getSimpleName();
+    private LocationManager mLocationManager = null;
+    private static final int LOCATION_INTERVAL = 1000;
+    private static final float LOCATION_DISTANCE = 10f;
+
+    private class LocationListener implements android.location.LocationListener {
+        Location mLastLocation;
+
+        public LocationListener(String provider) {
+            Log.i(TAG, "LocationListener " + provider);
+            mLastLocation = new Location(provider);
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.i(TAG, "onLocationChanged: " + location);
+            Toast.makeText(BaseApplication.application, "GPS 변경됨 : " + location, Toast.LENGTH_LONG).show();
+            mLastLocation.set(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.i(TAG, "onProviderDisabled: " + provider);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.i(TAG, "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.i(TAG, "onStatusChanged: " + provider);
+        }
+    }
+
+    LocationListener[] mLocationListeners = new LocationListener[]{
+            new LocationListener(LocationManager.GPS_PROVIDER),
+            new LocationListener(LocationManager.NETWORK_PROVIDER)
+    };
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "onStartCommand");
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onCreate() {
+        initializeLocationManager();
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 
+                    LOCATION_INTERVAL,
+                    LOCATION_DISTANCE,
+                    mLocationListeners[1]
+            );
+        
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 
+                    LOCATION_INTERVAL, 
+                    LOCATION_DISTANCE,
+                    mLocationListeners[0]
+            );
+        } catch (Exception exception) {
+            Toast.makeText(this, "GPS 정보를 받아 올 수 없습니다.", Toast.LENGTH_LONG).show();
+            exception.printStackTrace();
+        } 
+    }
+
+    private void initializeLocationManager() {
+        if (mLocationManager == null) {
+            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mLocationManager != null) {
+            for (LocationListener mLocationListener : mLocationListeners) {
+                try {
+                    mLocationManager.removeUpdates(mLocationListener);
+                } catch (Exception exception) {
+                    Toast.makeText(this, "알 수 없는 오류가 발생하였습니다.", Toast.LENGTH_LONG).show();
+                    exception.printStackTrace();
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/planet/nana/ui/main/MainActivity.java
+++ app/src/main/java/com/planet/nana/ui/main/MainActivity.java
@@ -1,9 +1,11 @@
 package com.planet.nana.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.planet.nana.R;
 import com.planet.nana.databinding.ActivityMainBinding;
+import com.planet.nana.service.LocationListenService;
 import com.planet.nana.ui.base.BaseActivity;
 
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
@@ -12,5 +14,12 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentBinding(R.layout.activity_main);
+
+        startLocationService();
+    }
+
+    private void startLocationService() {
+        Intent intent = new Intent(this, LocationListenService.class);
+        startService(intent);
     }
 }
