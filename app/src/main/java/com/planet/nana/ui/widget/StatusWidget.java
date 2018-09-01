a09db36a55002b7f7110e28dfd4f151d6daa2646
jspiner <jspiner@naver.com>
2018-09-01 19:48:52 +0900

    위젯 추가

---
+++ app/src/main/java/com/planet/nana/ui/widget/StatusWidget.java
@@ -0,0 +1,36 @@
+package com.planet.nana.ui.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import com.planet.nana.R;
+
+public class StatusWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.status_widget);
+        views.setTextViewText(R.id.appwidget_text, "EXAMPLE");
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+    }
+}
+
