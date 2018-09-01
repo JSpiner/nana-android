feb1667372e6c1459de5beb0014d7cbff92d37bc
jspiner <jspiner@naver.com>
2018-09-02 05:49:58 +0900

    더미 레이아웃 추가

---
+++ app/src/main/java/com/planet/nana/ui/main/ZoneViewHolder.java
@@ -0,0 +1,12 @@
+package com.planet.nana.ui.main;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class ZoneViewHolder extends RecyclerView.ViewHolder {
+
+    public ZoneViewHolder(View itemView) {
+        super(itemView);
+    }
+
+}
