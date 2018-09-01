1d31f8cba106b4837c993dcff2a7dfbb6e6e0b88
jspiner <jspiner@naver.com>
2018-09-01 16:47:46 +0900

    키값들을 저장하고 불러오는 Prefer 유틸 추가

---
+++ app/src/main/java/com/planet/nana/util/Prefer.java
@@ -0,0 +1,27 @@
+package com.planet.nana.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.planet.nana.BaseApplication;
+
+public final class Prefer {
+
+    public static final String KEY_LOGINED_ID = "keyLoginedId";
+
+    private static final String PREFER_NAME = "prefer";
+
+    private static SharedPreferences getSharedPreference() {
+        Context context = BaseApplication.application;
+        return context.getSharedPreferences(PREFER_NAME, Context.MODE_PRIVATE);
+    }
+
+    public static void putString(String key, String value) {
+        getSharedPreference().edit().putString(key, value).apply();
+    }
+
+    public static String getString(String key) {
+        return getSharedPreference().getString(key, null);
+    }
+
+}
