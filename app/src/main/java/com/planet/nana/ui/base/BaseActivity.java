6a3fcb3ec40296715220a7febbc97db5005e21c2
jspiner <jspiner@naver.com>
2018-09-01 17:43:59 +0900

    baseactivity를 상속받은 객체에서 뷰를 건들수 있게 binding을 protected로 변경

--- app/src/main/java/com/planet/nana/ui/base/BaseActivity.java
+++ app/src/main/java/com/planet/nana/ui/base/BaseActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 
 public abstract class BaseActivity<B extends ViewDataBinding> extends AppCompatActivity {
 
-    private B binding;
+    protected B binding;
 
     public void setContentBinding(int layoutResID) {
         binding = DataBindingUtil.setContentView(this, layoutResID);
