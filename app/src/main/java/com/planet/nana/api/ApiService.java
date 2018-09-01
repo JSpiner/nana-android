4dccaf019dedf4e6c4c6efbdf022f5de94513480
jspiner <jspiner@naver.com>
2018-09-01 19:24:15 +0900

    로그인 성공 직후 주소록을 서버로 보내는 기능 추가

--- app/src/main/java/com/planet/nana/api/ApiService.java
+++ app/src/main/java/com/planet/nana/api/ApiService.java
@@ -1,5 +1,7 @@
 package com.planet.nana.api;
 
+import java.util.List;
+
 import io.reactivex.Completable;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
@@ -12,6 +14,13 @@ public interface ApiService {
     Completable login(
             @Field("email") String email,
             @Field("password") String password
+    );
+
+    @FormUrlEncoded
+    @POST("/contact")
+    Completable addContact(
+            @Field("user_id") String userId,
+            @Field("contact") List<String> contactList
     );
 
 }
--- app/src/main/java/com/planet/nana/ui/login/LoginActivity.java
+++ app/src/main/java/com/planet/nana/ui/login/LoginActivity.java
@@ -1,19 +1,32 @@
 package com.planet.nana.ui.login;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import static android.provider.ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
-import android.widget.Toast;
+import android.util.Log;
 
 import com.planet.nana.R;
 import com.planet.nana.api.Api;
 import com.planet.nana.databinding.ActivityLoginBinding;
+import com.planet.nana.model.Contact;
 import com.planet.nana.ui.base.BaseActivity;
+import com.planet.nana.ui.main.MainActivity;
 
+import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 public class LoginActivity extends BaseActivity<ActivityLoginBinding> {
+
+    private static final String[] PROJECTION = new String[]{
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+            ContactsContract.CommonDataKinds.Phone.NUMBER
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,8 +58,55 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding> {
                 binding.userPw.getText().toString()
         ).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(() -> showToast("로그인 되셨습니다."),
+                .subscribe(() -> {
+                            showToast("로그인 되셨습니다.");
+                            uploadContacts();
+                        },
                         throwable -> showToast("id/pw를 확인해주세요."));
+    }
+
+    private void uploadContacts() {
+        loadContacts().map(Contact::getName)
+                .toList()
+                .flatMapCompletable(contactList -> Api.getInstance().addContact("1", contactList))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(() -> {
+                            showToast("주소록 동기화가 완료되었습니다.");
+                            startMainActivity();
+                        },
+                        throwable -> showToast("주소록 동기화를 실패하였습니다.")
+                );
+    }
+
+    private Observable<Contact> loadContacts() {
+        Observable<Contact> contactObservable = Observable.create(
+                emitter -> {
+                    ContentResolver resolver = getContentResolver();
+                    Cursor cursor = resolver.query(CONTENT_URI, PROJECTION, null, null, null);
+                    final int nameIndex = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+                    final int numberIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+
+                    String name;
+                    String number;
+                    while (cursor.moveToNext()) {
+                        name = cursor.getString(nameIndex);
+                        number = cursor.getString(numberIndex)
+                                .replace("-", "")
+                                .replace("+82", "");
+
+                        emitter.onNext(new Contact(name, number));
+                    }
+                    cursor.close();
+                    emitter.onComplete();
+                }
+        );
+        return contactObservable.observeOn(Schedulers.io());
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
