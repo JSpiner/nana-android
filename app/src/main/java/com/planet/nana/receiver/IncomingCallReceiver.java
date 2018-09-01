38dbc01b16d6857135eea5fcbe21d200916c0387
jspiner <jspiner@naver.com>
2018-09-02 00:27:19 +0900

    전화가 오면 1. 필터할 지도에 내위치가 있나 확인 2. 내위치가 포함된 그룹에 전화온 번호가 있나 확인. 해서 전화를 차단하도록 추가

--- app/src/main/java/com/planet/nana/receiver/IncomingCallReceiver.java
+++ app/src/main/java/com/planet/nana/receiver/IncomingCallReceiver.java
@@ -3,12 +3,21 @@ package com.planet.nana.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.telephony.TelephonyManager;
 import android.widget.Toast;
 
 import com.android.internal.telephony.ITelephony;
+import com.planet.nana.model.Contact;
+import com.planet.nana.model.Zone;
+import com.planet.nana.repository.ContactRepository;
+import com.planet.nana.repository.ZoneRepository;
+import com.planet.nana.service.LocationListenService;
 
 import java.lang.reflect.Method;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 
 public class IncomingCallReceiver extends BroadcastReceiver {
 
@@ -23,13 +32,51 @@ public class IncomingCallReceiver extends BroadcastReceiver {
     }
 
     private void onCallRinging(Context context, String number) {
-        if (isInBlackList(number)) {
-            endCurrentCall(context, number);
-        }
+        checkBlackList(context, number);
     }
 
-    private boolean isInBlackList(String number) {
-        return number.equals("TEST");
+    private void checkBlackList(Context context, String number) {
+        ZoneRepository.getInstance().getFilterZoneList()
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMapObservable(Observable::fromIterable)
+                .filter(this::isInMyLocation)
+                .filter(zone -> hasMyNumber(zone, number))
+                .subscribe(zone -> endCurrentCall(context, number));
+    }
+
+    private boolean isInMyLocation(Zone zone) {
+        Location myLocation = LocationListenService.lastTrackedLocation;
+
+        float[] result =new float[5];
+        Location.distanceBetween(
+                myLocation.getLatitude(),
+                myLocation.getLongitude(),
+                zone.getLatitude(),
+                zone.getLongitute(),
+                result
+        );
+
+        float distanceInKm = result[0] * 1000;
+        return distanceInKm < zone.getRadius();
+    }
+
+    private boolean hasMyNumber(Zone zone, String number) {
+        String replacedNumber = number.replace("-", "")
+                .replace("+82", "");
+
+        Contact contactInPhone = ContactRepository.getInstance()
+                .loadContacts()
+                .filter(contact -> contact.getNumber().equals(replacedNumber))
+                .singleOrError()
+                .onErrorReturnItem(new Contact(null, null))
+                .blockingGet();
+
+        for (Contact contact : zone.getContact()) {
+            if (contact.getName().equals(contactInPhone.getName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void endCurrentCall(Context context, String number) {
