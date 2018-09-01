70f11b34987a57776b65134626a84664212ce882
jspiner <jspiner@naver.com>
2018-09-02 00:53:07 +0900

    모든 activity에 자동으로 폰트가 적용되도록 baseactivity에 font 설정 추가

--- app/src/main/java/com/planet/nana/ui/base/BaseActivity.java
+++ app/src/main/java/com/planet/nana/ui/base/BaseActivity.java
@@ -1,9 +1,12 @@
 package com.planet.nana.ui.base;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public abstract class BaseActivity<B extends ViewDataBinding> extends AppCompatActivity {
 
@@ -15,5 +18,10 @@ public abstract class BaseActivity<B extends ViewDataBinding> extends AppCompatA
 
     protected void showToast(String text) {
         Toast.makeText(getBaseContext(), text, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
