566cdea7abbbc1272fb1958088c8611b6c1f5998
jspiner <jspiner@naver.com>
2018-09-01 21:11:27 +0900

    zone 조회 api 추가, Zone 모델 추가

--- app/src/main/java/com/planet/nana/api/ApiService.java
+++ app/src/main/java/com/planet/nana/api/ApiService.java
@@ -1,11 +1,17 @@
 package com.planet.nana.api;
 
+import com.planet.nana.model.Zone;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Completable;
+import io.reactivex.Single;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ApiService {
 
@@ -21,6 +27,11 @@ public interface ApiService {
     Completable addContact(
             @Field("user_id") String userId,
             @Field("contact") List<String> contactList
+    );
+
+    @GET("/zone/{user_id}")
+    Single<ArrayList<Zone>> getZoneList(
+            @Path("user_id") String userId
     );
 
 }
---
+++ app/src/main/java/com/planet/nana/model/Zone.java
@@ -0,0 +1,25 @@
+package com.planet.nana.model;
+
+public class Zone {
+
+    private int id;
+    private double latitude;
+    private double longitute;
+    private double radius;
+
+    public int getId() {
+        return id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitute() {
+        return longitute;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
