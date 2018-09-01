fd554798171fe01380f8f768310ba49f14f20320
jspiner <jspiner@naver.com>
2018-09-02 00:51:31 +0900

    font 추가

--- app/src/main/java/com/planet/nana/BaseApplication.java
+++ app/src/main/java/com/planet/nana/BaseApplication.java
@@ -2,6 +2,8 @@ package com.planet.nana;
 
 import android.app.Application;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 public class BaseApplication extends Application {
 
     public static BaseApplication application;
@@ -11,5 +13,11 @@ public class BaseApplication extends Application {
         super.onCreate();
 
         BaseApplication.application = this;
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/helvetica.ttc")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
     }
 }
