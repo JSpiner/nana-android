dbe4b95dc0c7feef3eef733aa90092a5c4575765
jspiner <jspiner@naver.com>
2018-09-01 17:45:55 +0900

    id/pw가 입력된경우 체크 로직 추가

--- app/src/main/java/com/planet/nana/ui/login/LoginActivity.java
+++ app/src/main/java/com/planet/nana/ui/login/LoginActivity.java
@@ -2,6 +2,8 @@ package com.planet.nana.ui.login;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.widget.Toast;
 
 import com.planet.nana.R;
 import com.planet.nana.databinding.ActivityLoginBinding;
@@ -13,5 +15,28 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding> {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentBinding(R.layout.activity_login);
+
+        init();
     }
+
+    private void init() {
+        binding.login.setOnClickListener(__ -> {
+            if (isValidInput()) {
+                requestLogin();
+            }
+            else {
+                Toast.makeText(this, "id/pw를 입력해주세요.", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private boolean isValidInput() {
+        return !TextUtils.isEmpty(binding.userId.getText()) &&
+                !TextUtils.isEmpty(binding.userPw.getText());
+    }
+
+    private void requestLogin() {
+        
+    }
+
 }
