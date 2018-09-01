c3e843adbdacd17e0597b2475c4252c33df37966
jspiner <jspiner@naver.com>
2018-09-01 16:50:52 +0900

    splash에서 로그인 기록 유무에따라 로그인페이지/메인페이지로 분기하도록 추가

--- app/src/main/java/com/planet/nana/ui/splash/SplashActivity.java
+++ app/src/main/java/com/planet/nana/ui/splash/SplashActivity.java
@@ -1,11 +1,20 @@
 package com.planet.nana.ui.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 
 import com.planet.nana.R;
 import com.planet.nana.databinding.ActivitySplashBinding;
 import com.planet.nana.ui.base.BaseActivity;
+import com.planet.nana.ui.login.LoginActivity;
+import com.planet.nana.ui.main.MainActivity;
+import com.planet.nana.util.Prefer;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Completable;
 
 public class SplashActivity extends BaseActivity<ActivitySplashBinding> {
 
@@ -13,5 +22,37 @@ public class SplashActivity extends BaseActivity<ActivitySplashBinding> {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentBinding(R.layout.activity_splash);
+
+        init();
+    }
+
+    private void init() {
+        Completable.timer(1000, TimeUnit.MILLISECONDS)
+                .subscribe(this::checkLogin);
+    }
+
+    private void checkLogin() {
+        if (isLogined()) {
+            startMainActivity();
+        }
+        else {
+            startLoginActivity();
+        }
+    }
+
+    private boolean isLogined() {
+        return !TextUtils.isEmpty(Prefer.getString(Prefer.KEY_LOGINED_ID));
+    }
+
+    private void startLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
